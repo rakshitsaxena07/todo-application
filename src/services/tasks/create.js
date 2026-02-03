4641677537196e4407e70a347ec2bc75bfd720ca
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-04 01:22:37 +0530

    feat(service):feat: implement task creation service with duplicate check

---
+++ src/services/tasks/create.js
@@ -0,0 +1,33 @@
+const { STATUS } = require('../../constants/taskConstants');
+const store = require('../../data/store');
+const crypto = require('crypto');
+const validateCreateTask = require('../../validators/taskValidator');
+
+const create = (data) => {
+    validateCreateTask(data);
+    const { title, description, priority } = data;
+
+    const isTaskExists = store.tasks.find((task) => task.title.toLowerCase() === title.toLowerCase());
+    if (isTaskExists) {
+        const error = new Error("Task with this title already exists");
+        error.status = 409;
+        error.code = "DUPLICATE_TASK";
+        throw error;
+    }
+
+    const now = new Date();
+    const task = {
+        id: crypto.randomUUID(),
+        title: title,
+        description: description,
+        status: STATUS.PENDING,
+        priority: priority,
+        createdAt: now,
+        updatedAt: now,
+    };
+
+    store.tasks.push(task);
+    return task;
+}
+
+module.exports = create;
