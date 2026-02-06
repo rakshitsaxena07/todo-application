b192f3e9cbed0183ca0e67aa3e03d862c3dc27e0
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-06 14:38:53 +0530

    delete unused files and folders

---
+++ src/routes/task.route.js
@@ -0,0 +1,11 @@
+// src/routes/task.route.js
+const express = require('express');
+const taskController = require("../controllers/task.controller");
+const validateMiddleware = require("../validators/taskValidator")
+
+const router = express.Router();
+
+router.post('/',validateMiddleware.validateCreateTask, taskController.createTask);
+router.get('/', taskController.getAllTask);
+router.patch('/:id',validateMiddleware.validateUpdateTask, taskController.updateTask);
+module.exports = router;
