165161ca3bc044b4bfa71562e1b74a1112b6b979
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-06 15:52:12 +0530

    fix: correct app.use catch-all syntax

--- src/app.js
+++ src/app.js
@@ -6,5 +6,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/v1/tasks', taskRoutes);
+app.use('/*', (req, res) => {
+    res.status(404).send('Not Found');
+});
 
 module.exports = app;
