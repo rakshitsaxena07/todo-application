07463b69e6f1ddbe469a93f3a3f42bbd317d862b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-06 14:35:46 +0530

    fix: valdation and status code

--- src/app.js
+++ src/app.js
@@ -6,10 +6,5 @@ const app = express();
 app.use(express.json());
 
 app.use('/v1/tasks', taskRoutes);
-app.use('*', (req, res) => {
-  res.status(404).json({
-    success: false,
-    message: 'Route not found',
-  });
-});
+
 module.exports = app;
--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,48 +1,53 @@
-const service = require("../services/task.service")
+const service = require('../services/task.service');
 
 const createTask = async (req, res) => {
-    try {
-        const data = await req.body
-        const task = await service.createTask(data);
-        res.status(201).json(task);
-    } catch (error) {
-        res.status(400).json({
-            "error": {
-                "code": "INVALID_TASK_DATA",
-                "message": error.message
-            }
-        })
-    }
-}
+  try {
+    const data = req.body;
+    const task = await service.createTask(data);
+
+    return res.status(201).json(task);
+  } catch (error) {
+    return res.status(error.status || 400).json({
+      error: {
+        code: 'INVALID_TASK_DATA',
+        message: error.message,
+      },
+    });
+  }
+};
+
 const getAllTask = async (req, res) => {
-    try {
-        const query = await req.query;
-        const tasks = await service.getAllTask(query);
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({
-            "error": {
-                "code": "INTERNAL_SERVER_ERROR",
-                "message": error.message
-            }
-        })
-    }
-}
+  try {
+    const query = req.query;
+    const tasks = await service.getAllTask(query);
+
+    return res.status(200).json(tasks);
+  } catch (error) {
+    return res.status(error.status || 500).json({
+      error: {
+        code: 'INTERNAL_SERVER_ERROR',
+        message: error.message,
+      },
+    });
+  }
+};
 
 const updateTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const data = req.body;
+  try {
+    const { id } = req.params;
+    const data = req.body;
 
-        const task = await service.updateTask(id, data);
-        res.status(200).json(task);
-    } catch (err) {
-        res.status(404).json({
-            "error": {
-                "code": "TASK_NOT_FOUND",
-                "message": err.message
-            }
-        })
-    }
-}
-module.exports = { createTask, updateTask, getAllTask };
+    const task = await service.updateTask(id, data);
+
+    return res.status(200).json(task);
+  } catch (error) {
+    return res.status(error.status || 404).json({
+      error: {
+        code: 'TASK_NOT_FOUND',
+        message: error.message,
+      },
+    });
+  }
+};
+
+module.exports = { createTask, getAllTask, updateTask };
