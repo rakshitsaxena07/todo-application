469d2f7c140e9a1e0234e021073bc5ef46303b32
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-06 12:39:55 +0530

    feat:add 404 handler

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.use('/v1/tasks', taskRoutes);
-
+app.use('*', (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
 module.exports = app;
