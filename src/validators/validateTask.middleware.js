816e5e8b8a99f1a0a678fb1167dcd6bb3d064cf7
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-06 12:51:32 +0530

    feat: add create Task schema for valdiation

--- src/validators/validateTask.middleware.js
+++ src/validators/validateTask.middleware.js
@@ -4,3 +4,12 @@ const priorityEnum = ['low', 'medium', 'high'];
 
 const titleSchema = z.string().trim().min(1,"Title is required").max(100,"Title must be at most 100 character");
 
+const descriptionSchema = z.string().trim().min(1, 'Description is required').max(500, 'Description must be at most 500 characters');
+
+const createTaskSchema = z.object({
+    title: titleSchema,
+    description: descriptionSchema,
+    status: z.enum(statusEnum).optional().default('pending'),
+    priority: z.enum(priorityEnum).optional().default('low'),
+});
+
