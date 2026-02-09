7bfc40d645e70a7d8978288c3d17032cf85ba445
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-09 12:10:28 +0530

    feat(app): integrate tasks table migration on app start

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 // src/app.js
 const express = require('express');
 const taskRoutes = require('./routes/task.route');
-
+const createTasksTable = require('./migrate')
 const app = express();
 app.use(express.json());
+createTasksTable()
 
 app.use('/v1/tasks', taskRoutes);
 app.use((req, res) => {
